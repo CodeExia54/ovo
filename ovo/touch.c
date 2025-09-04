c1f5ee8160372caf4b7ab766fcb14ae2c6e05d12
CodeExia54 <codeexia97@gmail.com>
2025-09-05 00:42:29 +0530

    Update touch.c

--- ovo/touch.c
+++ ovo/touch.c
@@ -336,14 +336,14 @@ int init_input_dev(void) {
 	}
 */
 
-	if(my_input_handle_event == NULL) {
-		my_input_handle_event = (void (*)(struct input_dev *, unsigned int, unsigned int, int))ovo_kallsyms_lookup_name("input_handle_event");
-	}
+	int ret = init_my_input_handle_event();
+if (ret) {
+    pr_err("[ovo] could not initialize input_handle_event\n");
+    unregister_kprobe(&input_event_kp);
+    unregister_kprobe(&input_inject_event_kp);
+    return ret;
+}
 
-	if (!my_input_handle_event) {
-		pr_err("[ovo] failed to find my_input_handle_event\n");
-		return -1;
-	}
 
 	pool = kvmalloc(sizeof(struct event_pool), GFP_KERNEL);
 	if (!pool) {
